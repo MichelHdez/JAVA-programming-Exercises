57f51e9290ab882772832f3d143749e41d2d13e2
Michel Hdez <michel01406@gmail.com>
2023-03-04 01:28:53 +0100

    Add files via upload

---
+++ Gestion-Empleados/src/main/java/com/gestion/empleados/excepciones/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.gestion.empleados.excepciones;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException {
+
+}
